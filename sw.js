9c03c132e6dcbc7c9da8b36f3ab5e2380b649fea
mahdishakerinia68-tech <mahdishakerinia68@gmail.com>
2026-08-30 21:19:04 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const CACHE="hesabdar-3.0-offline-v1";
-const ASSETS=["./","./index.html","./style.css","./app.js","./capacitor-local-notifications-bridge.js","./manifest.json","./sw.js"];
+const CACHE="hesabdar-3.1-offline-v1";
+const ASSETS=["./",
+      "./logo.png","./index.html","./style.css","./app.js","./capacitor-local-notifications-bridge.js","./manifest.json","./sw.js"];
 
 self.addEventListener("install", event=>{
   event.waitUntil(
